a9892ad7ed8e65b1fad849a7eca24392aaf61a08
pablolleite88 <pablol.leite88@gmail.com>
2025-08-03 22:53:46 -0300

    acrescimo de validacoes

--- src/main/java/com/rocketseat/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/rocketseat/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,7 +2,10 @@ package com.rocketseat.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 
 @Data
@@ -10,8 +13,14 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^[^\\s]+$", message = "Username não deve conter espaços")
     private String username;
+
+    @Email(message = "Deve conter um email válido")
     private String email;
+
+    @Length(min = 6, max = 20, message = "A senha deve ter entre 6 e 20 caracteres")
     private String password;
     private String description;
     private String curriculum;
