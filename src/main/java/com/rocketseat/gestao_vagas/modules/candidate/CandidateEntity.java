f5bf7e3d998cb2d9694df750d3a3c9f9d2ee104e
Pablo <pabloleite@outlook.com>
2025-08-05 21:38:06 -0300

    docker

--- src/main/java/com/rocketseat/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/rocketseat/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,16 +1,24 @@
 package com.rocketseat.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
-
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.UUID)
     private UUID id;
     private String name;
 
@@ -24,6 +32,9 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 
 
